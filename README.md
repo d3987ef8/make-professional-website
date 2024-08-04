e904801411ced9e13d051b42c50bbba76414112f
Jack Murray <info@jackmurray.com>
2024-08-04 18:19:18 +1000

    make things a bit more modular

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
-# make-resume
+# make-professional-website (mpw)
 
-This is a Python package that provides the CLI script `make-professional-website`.
+This is a Python package that provides the CLI scripts
+`make-professional-website` and `mpw` for short (they do the same thing).
 
 It is intended for anybody who prefers to maintain their data in plain text YAML files.
+
+## Why make-professional-website?
+
+  1. *Ease of Use for Beginners*: The simplicity of using YAML over HTML for beginners is a significant advantage. Many people find HTML daunting initially, so this package offers a simpler alternative. There is a focus on ease of use, especially for those new to coding or those who need to create simple websites quickly.
+  2. *Niche Market*: There seems to be a niche market for this kind of tool. Beginners who want to create professional-looking websites without diving deep into HTML and CSS, as well as those who prefer a more old-school approach to web development, could find this framework very useful.
+  3. *Unique Approach*: This tool's focus on minimal dependencies and code security, combined with the classical approach to frontend work, differentiates it from many modern frameworks. This could appeal to developers who are cautious about the complexity associated with many modern frontend technologies.
+  4. Potential Users:
+    - Hobbyists and DIYers: Individuals who want to create personal websites or projects.
+    - Small Business Owners: People who need a simple, professional web presence without the overhead of learning complex web technologies.
+    - Educators and Students: Educational institutions might find this tool useful for teaching the basics of web development without overwhelming students.
+
+## How can I get started?
 
 It looks for the following files in the current directory. All files are optional.
 
     1. *resume.yaml* - This is the landing page and contains your professional resume or CV.
     2. *concepts.yaml* - This is a page to suggest new words or acronyms for concepts that don't have obvious definitions yet.
-    3. *contact.yaml* - A contact page, powered by Devro LABS.
+    3. *contact.yaml* - A contact form.
 
 Examples of these YAML files are provided in this repository can be found
 [here](https://github.com/d3987ef8/make-professional-website/tree/main/examples).
@@ -36,9 +49,3 @@ This project depends on:
     - Jinja2 Python package
     - pyyaml Python package
     - FormSubmit, a service provided by Devro LABS.
-
-## Contributing
-
-To begin making your own templates for pages, I recommend checking out the existing HTML templates
-[here](https://github.com/d3987ef8/make-professional-website/tree/main/src/make_professional_website/)
-to reference examples.
--- src/make_professional_website/make_professional_website.py
+++ src/make_professional_website/make_professional_website.py
@@ -1,7 +1,10 @@
 from yaml import safe_load
 from jinja2 import Template
 from pathlib import Path
+import sys
 import os
+
+sys.tracebacklimit = 0
 
 TEMPLATE_PATH = Path(os.path.os.path.realpath(__file__)).parent
 
@@ -12,31 +15,41 @@ def make_professional_website():
     try:
         with globals_path.open("r") as f:
             globals = safe_load(f)
-    except:
+    except Exception as e:
         print("Error: Could not load globals.yaml")
+        print(e)
         return 1
     if "Domains" not in globals:
         print("Error: globals.yaml must contain at least one domain.")
         return 1
 
-    for g in globals:
-        os.environ[g] = globals[g]
-
     generated_pdf_html = False
     for domain in globals["Domains"]:
-        os.environ["Domain"] = domain
         globals["Domain"] = domain
+        sitemap_paths = []
 
-        for template_path in TEMPLATE_PATH.glob("*.html")
-            name = template_path.stem
+        for template_path in TEMPLATE_PATH.glob("**/*.html"):
+            if len(template_path.parts) == 1 + len(TEMPLATE_PATH.parts):
+                name = template_path.stem
+                output_path = f"{name}.html"
+                should_mkdir = False
+            else:
+                name = template_path.parts[-2]
+                output_path = f"{name}/{template_path.parts[-1]}"
+                should_mkdir = True
             yaml_path = Path(f"{name}.yaml")
             if not yaml_path.exists():
                 continue
             try:
                 with yaml_path.open("r") as f:
-                    yaml = safe_load(f)
-            except:
+                    raw_txt = f.read()
+                    for g in globals:
+                        if isinstance(globals[g], str):
+                            raw_txt = raw_txt.replace(f"__{g}__", globals[g])
+                    yaml = safe_load(raw_txt)
+            except Exception as e:
                 print(f"Error: Could not load {name}.yaml")
+                print(e)
                 return 1
 
             print(f"[+] Loaded {name}.yaml")
@@ -44,11 +57,12 @@ def make_professional_website():
             try:
                 with template_path.open("r") as f:
                     template = Template(f.read(), autoescape=True)
-            except:
-                print(f"Could not load {name}.html")
+            except Exception as e:
+                print(f"Could not load {output_path} template")
+                print(e)
                 return 1
 
-            print(f"[+] Loaded {name}.html")
+            print(f"[+] Loaded {output_path} template")
 
             html = template.render({
                 name: yaml,
@@ -56,7 +70,17 @@ def make_professional_website():
                 "pdf": False,
             })
 
-            output_path = Path(domain) / ("index.html" if name == LANDING_PAGE_TEMPLATE_NAME else f"{name}.html")
+            if name == LANDING_PAGE_TEMPLATE_NAME:
+                sitemap_paths.append("")
+            elif output_path.endswith("index.html"):
+                sitemap_paths.append(f"{output_path.parent}/")
+            else:
+                sitemap_paths.append(output_path)
+
+            output_path = Path(domain) / ("index.html" if name == LANDING_PAGE_TEMPLATE_NAME else output_path)
+
+            if should_mkdir:
+                output_path.parent.mkdir(exist_ok=True)
 
             with output_path.open("w") as f:
                 f.write(html)
@@ -73,4 +97,17 @@ def make_professional_website():
                     f.write(pdf_html)
                 print("[+] Generated pdf.html")
                 generated_pdf_html = True
+        try:
+            with Path(TEMPLATE_PATH / "sitemap.xml").open("r") as f:
+                template = Template(f.read(), autoescape=True)
+        except Exception as e:
+            print("Could not load sitemap.xml template")
+            print(e)
+            return 1
+        sitemap_xml = template.render({
+            "paths": sitemap_paths,
+            "globals": globals,
+        })
+        with (Path(domain) / "sitemap.xml").open("w") as f:
+            f.write(sitemap_xml)
     return 0
